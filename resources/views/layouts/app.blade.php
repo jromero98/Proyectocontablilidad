1c5dd071ded571fa8bc0bcd6a50497c4172fc94a
worrabroshnot <juank11pin@hotmail.com>
2017-03-13 20:10:34 -0500

    Welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Grateful Dogs</title>
+    <title>ASOVIZ</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
