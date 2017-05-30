430a6b023665e93ac026df3fd2a6ef2f457569d7
Jorge Romero <jromero199@gmail.com>
2017-05-30 00:09:23 -0500

    Prueba 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="panel panel-default">
-                <div class="panel-heading text-center">Hola {{ Auth::user()->name }} Bienvenido a {{$vivero->Nom_vivero}}</div>
+                <div class="panel-heading text-center">Hola {{ Auth::user()->name }} Bienvenido a {{$vivero->nom_vivero}}</div>
 
                 <div class="panel-body">
             @include('estadistica')
