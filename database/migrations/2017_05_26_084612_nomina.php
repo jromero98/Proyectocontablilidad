c640266e96f10d911dadbfeaea399aefa738461a
Jorge Romero <jromero199@gmail.com>
2017-05-29 23:14:17 -0500

    Prueba ruta9

--- database/migrations/2017_05_26_084612_nomina.php
+++ database/migrations/2017_05_26_084612_nomina.php
@@ -36,7 +36,7 @@ class Nomina extends Migration
         });
       
         DB::unprepared("
-          CREATE PROCEDURE `crearfc` (IN `idarticulo` VARCHAR(5), IN `idfactura` INT, IN `cantidad` INT, IN `precio_compra` INT, IN `precio_venta` INT, IN `precio` INT)  NO SQL
+          CREATE  OR REPLACE FUNCTION `crearfc` (IN `idarticulo` VARCHAR(5), IN `idfactura` INT, IN `cantidad` INT, IN `precio_compra` INT, IN `precio_venta` INT, IN `precio` INT)  NO SQL
           IF NOT EXISTS ( SELECT `idarticulo`, `idfactura`, `cantidad`, `precio_venta`, `descuento` 
           FROM detalle_factura
           WHERE detalle_factura.idarticulo = idarticulo and detalle_factura.idfactura=idfactura) THEN
@@ -45,7 +45,7 @@ class Nomina extends Migration
           SELECT 'Este Detalle ya existe en la base de datos!';
           END IF$$
 
-          CREATE PROCEDURE `crearfv` (IN `idarticulo` VARCHAR(5), IN `idfactura` INT, IN `cantidad` INT, IN `precio_venta` INT, IN `descuento` INT, IN `precio` INT)  NO SQL
+          CREATE  OR REPLACE FUNCTION `crearfv` (IN `idarticulo` VARCHAR(5), IN `idfactura` INT, IN `cantidad` INT, IN `precio_venta` INT, IN `descuento` INT, IN `precio` INT)  NO SQL
           IF NOT EXISTS ( SELECT `idarticulo`, `idfactura`, `cantidad`, `precio_venta`, `descuento` 
           FROM detalle_factura
           WHERE detalle_factura.idarticulo = idarticulo and detalle_factura.idfactura=idfactura) THEN
@@ -54,7 +54,7 @@ class Nomina extends Migration
           SELECT 'Este Detalle ya existe en la base de datos!';
           END IF$$
 
-          CREATE PROCEDURE `cuentastotal` (IN `fecha` DATE)  NO SQL
+          CREATE  OR REPLACE FUNCTION `cuentastotal` (IN `fecha` DATE)  NO SQL
           SELECT `fecha_nomina`, sum(diastrabajados*salario/30)as Sueldos , sum(salario*1.25*horased/240+salario*1.55*horasen/240)as Horasextras, sum(auxtransportes) as axtrans, sum(auxalimentos) as axali,sum(bonificaciones)as Bonificaciones,sum(comisiones) as Comisiones, sum(aporteeps) as aporteseps, sum(aportepension) as aportespensiones, sum(aportefondoempleados)as fondoempleados, sum(libranza)as libranza, sum(embargos)as embargos, sum(retencionfuente) as retencion, sum((diastrabajados*(salario/30)+auxtransportes+bonificaciones+comisiones+auxalimentos+salario*1.25*horased/240+salario*1.55*horasen/240)-(aporteeps+aportepension+aportefondoempleados+libranza+embargos+retencionfuente)) as Total FROM `nomina` WHERE fecha_nomina=fecha GROUP by fecha_nomina$$
 
           CREATE PROCEDURE `totales` (IN `fecha` DATE)  NO SQL
